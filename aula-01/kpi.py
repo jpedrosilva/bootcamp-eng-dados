a702aee3249878c02ba7bf67b11885fc08c5b335
Joao Pedro <jpfdsilva@hotmail.com>
2024-02-19 18:46:23 -0300

    update do código da aula 01

--- aula-01/kpi.py
+++ aula-01/kpi.py
@@ -1,12 +1,26 @@
+# Bibliotecas (nesse caso apenas para o formato da moeda)
+import locale
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
 # Constante do acréscimo do bônus
 CONST_BONUS = 1000
 
 # Funções
 def calc_bonus(salario: float, bonus: float):
-    x = CONST_BONUS + salario * bonus
-    return print(f'Olá {nome}, o seu bônus foi de R${x}')
+    x = locale.currency(CONST_BONUS + salario * bonus, grouping=True)
+    return print(f'Olá {nome}, o seu bônus foi de {x}.')
 
-def check_float(x):
+def check_nome(x: str):
+    try:
+        if len(x) <= 2 or not x.replace(' ', '').isalpha():
+            raise Exception()
+        else:
+            return (x)
+    except:
+        raise Exception('Nome inválido, por favor, refaça a operação.')
+
+def check_float(x: float):
     try:
         y = float(x.replace(',', '.'))
         if y < 0:
@@ -17,7 +31,7 @@ def check_float(x):
         raise Exception('Número inválido, por favor, refaça a operação.')
 
 # Input de variáveis + check
-nome = input('Digite o seu nome: ')
+nome = check_nome(input('Digite o seu nome: '))
 salario = check_float(input('Digite o seu salário: '))
 bonus = check_float(input('Digite o seu bônus: '))
 
