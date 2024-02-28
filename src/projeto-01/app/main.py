5650349f90afb55d469a768c78478309fd581f65
Joao Pedro <jpfdsilva@hotmail.com>
2024-02-28 13:09:34 -0300

    build: adição das tabelas de benchmark e resultados dos dataframes

--- src/projeto-01/app/main.py
+++ src/projeto-01/app/main.py
@@ -34,7 +34,6 @@ def load_csv_weather_stations() -> None:
         load_table_postgre(
             df=load_csv_to_dataframe(),
             table="raw_weather_stations",
-            mode="replace",
             schema="public",
         )
         print("O arquivo foi carregado no banco de dados.")
@@ -47,10 +46,10 @@ def load_csv_weather_stations() -> None:
 def main() -> None:
     try:
         # Testa a conexão com o banco de dados
-        # test_connection_postgre()
+        test_connection_postgre()
 
         # Carrega as informações para o banco de dados
-        # load_csv_weather_stations()
+        load_csv_weather_stations()
 
         # Executa a função de extração
         query = "SELECT * FROM public.raw_weather_stations"
@@ -64,15 +63,36 @@ def main() -> None:
         time_start = perf_counter()
         df_polars = aggregate_df_using_polars(df_pd, groupby_col, measure_col)
         time_polars = perf_counter() - time_start
-        print(df_polars)
-        print(f"Polars took: {time_polars:.2f} seconds.")
 
         # Executa a função de agg usando DuckDB
         time_start = perf_counter()
         df_duckdb = aggregate_df_using_duckdb(df_pd, groupby_col, measure_col)
         time_duckdb = perf_counter() - time_start
-        print(df_duckdb)
-        print(f"DuckDB took: {time_duckdb:.2f} seconds.")
+
+        # Criando tabela de benchmark no banco de dados
+        data_benchmark = {
+            "framework": ["Polars", "DuckDB"],
+            "time_elapsed": [time_polars, time_duckdb],
+        }
+        df_benchmark = pd.DataFrame(data=data_benchmark)
+        df_benchmark["dataset"] = "1MM rows"
+
+        load_table_postgre(
+            df=df_benchmark, table="benchmark_frameworks", schema="public"
+        )
+
+        # Leitura da tabela de benchmark
+        query_bench = "SELECT * FROM public.benchmark_frameworks"
+        df_bench = extract_data_pd(create_connection_string_postgre(), query_bench)
+        print(df_bench)
+
+        # Subindo a tabela com o dataframe tratado para o banco de dados
+        load_table_postgre(df=df_duckdb, table="tb_aggregated_data", schema="public")
+
+        # Leitura da tabela agregada
+        query_agg = "SELECT * FROM tb_aggregated_data"
+        df_agg = extract_data_pd(create_connection_string_postgre(), query_agg)
+        print(df_agg)
 
     except Exception as e:
         print(e)
